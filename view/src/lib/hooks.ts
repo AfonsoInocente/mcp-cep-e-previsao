62750ab57092118d306d5a39cf56b384a18a8735
Afonso Inocente <afonsohenriqueinocente@gmail.com>
2025-08-15 16:21:17 -0300

    feat: create full workflow + view

--- view/src/lib/hooks.ts
+++ view/src/lib/hooks.ts
@@ -1,5 +1,5 @@
 import { client } from "./rpc";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 /**
